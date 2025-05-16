7436d3c2a477db00d1a5e6c4e880ebd019bf6fc0
Ankit Kumar <ankitkumar.iitp09@gmail.com>
2025-05-16 12:32:05 +0530

    Fix build for Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src',
-      '@assets': '/src/assets'
-    }
+      "@": "/src",
+      "@assets": "/src/assets",
+    },
   },
   server: {
     fs: {
-      allow: ['..']
-    }
-  }
-})
+      allow: [".."],
+    },
+  },
+});
